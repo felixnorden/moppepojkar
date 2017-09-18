abd5f97c02a743d0a725b5e96edeb0883432b021
Nightingale <kevin.solovjov@gmail.com>
2017-09-18 20:34:54 +0200

    Fixed dependency error
    App opens normally now.

--- src/app/WirelessIno/app/src/main/java/se/sics/sse/fresta/wirelessino/MainActivity.java
+++ src/app/WirelessIno/app/src/main/java/se/sics/sse/fresta/wirelessino/MainActivity.java
@@ -1,9 +1,10 @@
 package se.sics.sse.fresta.wirelessino;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
