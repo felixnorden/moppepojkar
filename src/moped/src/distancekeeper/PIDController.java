aa947170b134f44b45a792ade1434644ce7a7c81
Emil Jansson <emiljanQ3@hotmail.com>
2017-09-18 17:14:09 +0200

    Refactored PIDcontroller and added some ducumentation

--- src/moped/src/distancekeeper/PIDController.java
+++ src/moped/src/distancekeeper/PIDController.java
@@ -6,11 +6,72 @@ package distancekeeper;
 public class PIDController {
     private double targetValue;
     private double constantP;
+    private double constantIRelation;
     private double constantI;
+    private double constantDRelation;
     private double constantD;
 
     private double integralSum = 0;
     private Double lastError = Double.NaN;
+
+    /**
+     * This is the PIDController constructor. It creates a new PIDController with custom constants which
+     * @param targetValue
+     * @param constantP
+     * @param constantIRelation
+     * @param constantDRelation
+     */
+
+    public PIDController(double targetValue, double constantP, double constantIRelation, double constantDRelation) {
+        this.constantP = constantP;
+        this.constantIRelation = constantIRelation;
+        this.constantI = this.constantP * constantIRelation;
+        this.constantDRelation = constantDRelation;
+        this.constantD = this.constantP * constantDRelation;
+        this.targetValue = targetValue;
+    }
+
+    public double getConstantP() {
+        return constantP;
+    }
+
+    public void setConstantP(double constantP) {
+        this.constantP = constantP;
+        this.constantI = constantP * constantIRelation;
+        this.constantD = constantP * constantDRelation;
+    }
+
+    public double getConstantI() {
+        return constantI;
+    }
+
+    public void setConstantIRelation(double constantIRelation) {
+        this.constantIRelation = constantIRelation;
+    }
+
+    public double getConstantD() {
+        return constantD;
+    }
+
+    public void setConstantDRelation(double constantDRelation) {
+        this.constantDRelation = constantDRelation;
+    }
+
+    public double getTargetValue() {
+        return targetValue;
+    }
+
+    public void setTargetValue(double targetValue) {
+        this.targetValue = targetValue;
+    }
+
+    /**
+     * This method evaluates the proper acceleration value to minimize error, where error is the difference between the current value and the target value.
+     * The method is dependant on the P-, I- and D-constants and these variables have to be calibrated to maximize the evaluations accuracy.
+     * @param currentValue Current value to be compared to the target value.
+     * @param deltaTime Elapsed time since the last method call.
+     * @return Acceleration value to be fed back into the loop. The first time this method is called it will always return 0.
+     */
 
     public double evaluation(double currentValue, double deltaTime){
         double currentError = (targetValue - currentValue);
@@ -27,56 +88,16 @@ public class PIDController {
         }
     }
 
-    private double decideD(double currentError, double deltaTime) {
-        return (currentError - lastError) * constantD / deltaTime;
+    private double decideP(double currentError) {
+        return currentError * constantP;
     }
-
 
     private double decideI(double currentError, double deltaTime) {
         integralSum += currentError * constantI * deltaTime;
         return integralSum;
     }
 
-    private double decideP(double currentError) {
-        return currentError * constantP;
-    }
-
-    public PIDController(double targetValue, double constantP, double constantIRelation, double constantDRelation) {
-        this.constantP = constantP;
-        this.constantI = this.constantP * constantIRelation;
-        this.constantD = this.constantP * constantDRelation;
-        this.targetValue = targetValue;
-    }
-
-    public double getConstantP() {
-        return constantP;
-    }
-
-    public void setConstantP(double constantP) {
-        this.constantP = constantP;
-    }
-
-    public double getConstantI() {
-        return constantI;
-    }
-
-    public void setConstantI(double constantI) {
-        this.constantI = constantI;
-    }
-
-    public double getConstantD() {
-        return constantD;
-    }
-
-    public void setConstantD(double constantD) {
-        this.constantD = constantD;
-    }
-
-    public double getTargetValue() {
-        return targetValue;
-    }
-
-    public void setTargetValue(double targetValue) {
-        this.targetValue = targetValue;
+    private double decideD(double currentError, double deltaTime) {
+        return (currentError - lastError) * constantD / deltaTime;
     }
 }
--- src/moped/src/distancekeeper/distancekeepertest/PIDControllerTest.java
+++ src/moped/src/distancekeeper/distancekeepertest/PIDControllerTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 class PIDControllerTest{
 
-
+    //TODO Would like to create a graph showing the pid in action.
 
     @Test
     void evaluation() {
@@ -33,8 +33,33 @@ class PIDControllerTest{
             currentSpeedOfChange = currentSpeedOfChange + throttle - currentSpeedOfChange * 0.1;
             currentValue+= currentSpeedOfChange;
             double error = currentValue-targetValue;
-            System.out.println(error);
+            System.out.println("Error 0: " + error);
+            System.out.println("throttle 0: " + throttle);
             assertTrue(Math.abs(error) < lastError || Math.abs(error) < 2);
+            lastError = Math.abs(error);
+
+        }
+
+    }
+
+    @Test
+    void evaluation1() {
+        System.out.println("\n\n\n\n");
+        double targetValue = 50;
+        PIDController pid = new PIDController(targetValue, 0.5, 1/10, 1.5);
+        double currentValue = 0;
+        double currentSpeedOfChange = 1;
+        double lastError = 51;
+        double targetSpeedOfChange = 10;
+
+        for (int i=0; i<100; i++){
+            double throttle = pid.evaluation(currentValue,1);
+            currentSpeedOfChange = currentSpeedOfChange + throttle - currentSpeedOfChange * 0.1;
+            currentValue+= currentSpeedOfChange - targetSpeedOfChange;
+            double error = currentValue-targetValue;
+            System.out.println("Error 1: " + error);
+            System.out.println("throttle 1: " + throttle);
+            //assertTrue(Math.abs(error) < lastError || Math.abs(error) < 2);
             lastError = Math.abs(error);
 
         }
