0946f6066790801311432f668b6295936125a20e
Felix Nordén <felixnorden@gmail.com>
2017-09-19 09:56:22 +0200

    Add boilerplate implementation

--- src/moped/src/main/java/core/behaviour_states/states/Manual.java
+++ src/moped/src/main/java/core/behaviour_states/states/Manual.java
@@ -1,4 +1,22 @@
 package core.behaviour_states.states;
 
+import core.action_strategies.ActionStrategy;
+import core.action_strategies.RemoteController;
+
 public class Manual implements BehaviourState {
+
+    private ActionStrategy controller;
+
+    public Manual() {
+
+    }
+
+    public void init(ActionStrategy controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
