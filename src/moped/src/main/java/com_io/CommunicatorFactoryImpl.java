8d0c1c65e6585a008434df27c4285db24bf001c2
AntonKimfors <antonkimfors@hotmail.com>
2017-09-19 09:12:07 +0200

    Remove misplaced methods

--- src/moped/src/main/java/com_io/CommunicatorFactoryImpl.java
+++ src/moped/src/main/java/com_io/CommunicatorFactoryImpl.java
@@ -1,8 +1,5 @@
 package com_io;
 
 public class CommunicatorFactoryImpl implements CommunicatorFactory {
-    private comInstance CommunicationsMediator;
-
-    public ExternelCommunicator getComInstance();
-
+  
 }
