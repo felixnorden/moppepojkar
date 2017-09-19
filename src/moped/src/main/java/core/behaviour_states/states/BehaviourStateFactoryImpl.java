97beca6ad9a734a17be604a1aa6ee54a957f0046
Felix Nordén <felixnorden@gmail.com>
2017-09-19 09:15:27 +0200

    Add singleton implementation

--- src/moped/src/main/java/core/behaviour_states/states/BehaviourStateFactoryImpl.java
+++ src/moped/src/main/java/core/behaviour_states/states/BehaviourStateFactoryImpl.java
@@ -1,6 +1,9 @@
 package core.behaviour_states.states;
 
 public class BehaviourStateFactoryImpl implements BehaviourStateFactory{
+
+    private static final BehaviourStateFactory INSTANCE = new BehaviourStateFactoryImpl();
+
     @Override
     public BehaviourState createManualBehaviour() {
         return new Manual();
@@ -10,4 +13,7 @@ public class BehaviourStateFactoryImpl implements BehaviourStateFactory{
     public BehaviourState createAdaptiveCruiseControlBehaviour() {
         return new AdaptiveCruiseControl();
     }
+
+    private BehaviourStateFactoryImpl() {}
+
 }
