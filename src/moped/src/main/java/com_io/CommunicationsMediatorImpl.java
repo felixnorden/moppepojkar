99273215ed68f6ac39d7dbca8d2958020bdaf9ee
AntonKimfors <antonkimfors@hotmail.com>
2017-09-19 09:07:07 +0200

    add implementation requirements

--- src/moped/src/main/java/com_io/CommunicationsMediatorImpl.java
+++ src/moped/src/main/java/com_io/CommunicationsMediatorImpl.java
@@ -1,4 +1,8 @@
 package com_io;
 
 public class CommunicationsMediatorImpl implements CommunicationsMediator{
+
+    private Map<DatRecievers> subscribers;
+
+    private void directPacket(int[] data, Direction direction);
 }
