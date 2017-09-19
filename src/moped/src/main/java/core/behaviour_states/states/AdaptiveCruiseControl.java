281a227fd5bedf275fdbc30498f0d9b400c3268c
Felix Nordén <felixnorden@gmail.com>
2017-09-19 09:56:22 +0200

    Add boilerplate implementation

--- src/moped/src/main/java/core/behaviour_states/states/AdaptiveCruiseControl.java
+++ src/moped/src/main/java/core/behaviour_states/states/AdaptiveCruiseControl.java
@@ -1,4 +1,21 @@
 package core.behaviour_states.states;
 
+import core.action_strategies.ActionStrategy;
+
 public class AdaptiveCruiseControl implements BehaviourState {
+
+    private ActionStrategy currentStrategy;
+
+    // Remove?
+    private ActionStrategy emgyStopStrategy;
+
+    @Override
+    public void run() {
+
+    }
+
+    // Parameters?
+    private ActionStrategy determineStrategy() {
+        return null;
+    }
 }
