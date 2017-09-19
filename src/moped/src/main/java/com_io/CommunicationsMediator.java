1bfa895830f52ca352d131af2f50f1c03b636f39
AntonKimfors <antonkimfors@hotmail.com>
2017-09-19 08:54:55 +0200

    add implementation requirements

--- src/moped/src/main/java/com_io/CommunicationsMediator.java
+++ src/moped/src/main/java/com_io/CommunicationsMediator.java
@@ -1,4 +1,9 @@
 package com_io;
 
 public interface CommunicationsMediator {
+
+    public void transmitData(int[] data, Direction direction){}
+    public void inputReciever(InputReciever reciever, Direction direction){}
+    public void unsubscribe(Inputreciever reciever){}
+
 }
