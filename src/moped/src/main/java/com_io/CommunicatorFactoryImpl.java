7e36e27df175040155bcbc4faf7a89d74c633d17
AntonKimfors <antonkimfors@hotmail.com>
2017-09-19 09:04:26 +0200

    add implementation requirements

--- src/moped/src/main/java/com_io/CommunicatorFactoryImpl.java
+++ src/moped/src/main/java/com_io/CommunicatorFactoryImpl.java
@@ -1,4 +1,8 @@
 package com_io;
 
 public class CommunicatorFactoryImpl implements CommunicatorFactory {
+    private comInstance CommunicationsMediator;
+
+    public ExternelCommunicator getComInstance();
+
 }
