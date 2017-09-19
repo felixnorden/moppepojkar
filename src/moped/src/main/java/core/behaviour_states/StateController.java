5575c547423f0f15cfcdc49dca1f7dc50c7c0b33
Felix Nordén <felixnorden@gmail.com>
2017-09-19 10:10:39 +0200

    Add boilerplate implementation

--- src/moped/src/main/java/core/behaviour_states/StateController.java
+++ src/moped/src/main/java/core/behaviour_states/StateController.java
@@ -1,4 +1,24 @@
 package core.behaviour_states;
 
+import core.behaviour_states.states.BehaviourState;
+
 public class StateController {
+    private BehaviourState currentState;
+
+    public StateController() {
+
+    }
+
+    // Inject anything?
+    public void init() {
+
+    }
+
+    public BehaviourState getCurrentState() {
+        return this.currentState;
+    }
+
+    public void setNewState(BehaviourState newState) {
+        this.currentState = newState;
+    }
 }
