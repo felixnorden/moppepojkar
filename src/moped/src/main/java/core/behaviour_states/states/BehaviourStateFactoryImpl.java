5052d2cc1ae0a7dcfacfc34448621cb7e3ceba18
Felix Nordén <felixnorden@gmail.com>
2017-09-19 08:53:45 +0200

    Add default factory implementation

--- src/moped/src/main/java/core/behaviour_states/states/BehaviourStateFactoryImpl.java
+++ src/moped/src/main/java/core/behaviour_states/states/BehaviourStateFactoryImpl.java
@@ -1,4 +1,13 @@
 package core.behaviour_states.states;
 
 public class BehaviourStateFactoryImpl implements BehaviourStateFactory{
+    @Override
+    public BehaviourState createManualBehaviour() {
+        return new Manual();
+    }
+
+    @Override
+    public BehaviourState createAdaptiveCruiseControlBehaviour() {
+        return new AdaptiveCruiseControl();
+    }
 }
