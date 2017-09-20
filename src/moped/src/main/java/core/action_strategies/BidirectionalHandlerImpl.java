43b835294e61f1ff10853f3b9ebdeed468b87f02
Felix Nordén <felixnorden@gmail.com>
2017-09-20 10:56:24 +0200

    Add strategy base implementation

--- src/moped/src/main/java/core/action_strategies/BidirectionalHandlerImpl.java
+++ src/moped/src/main/java/core/action_strategies/BidirectionalHandlerImpl.java
@@ -1,4 +1,21 @@
 package core.action_strategies;
 
-public class BidirectionalHandlerImpl {
+public class BidirectionalHandlerImpl implements BidirectionalHandler {
+    private ActionStrategy latitudeStrategy;
+    private ActionStrategy longitudeStrategy;
+
+    public BidirectionalHandlerImpl(ActionStrategy latitudeStrategy, ActionStrategy longitudeStrategy) {
+        this.latitudeStrategy = latitudeStrategy;
+        this.longitudeStrategy = longitudeStrategy;
+    }
+
+    @Override
+    public double takeLatitudeAction() {
+        return latitudeStrategy.takeAction();
+    }
+
+    @Override
+    public double takeLongitudeAction() {
+        return longitudeStrategy.takeAction();
+    }
 }
--- src/moped/src/main/java/core/action_strategies/RemoteController.java
+++ src/moped/src/main/java/core/action_strategies/RemoteController.java
@@ -1,4 +1,8 @@
 package core.action_strategies;
 
 public class RemoteController implements ActionStrategy {
+    @Override
+    public double takeAction() {
+        return 0;
+    }
 }
