e5b0f8fc2b8a843186ee1c7660d56945312f3af2
Felix Nordén <felixnorden@gmail.com>
2017-09-20 17:42:54 +0200

    Add start implementation for ACC

--- src/moped/src/main/java/core/action_strategies/ActionStrategyFactoryImpl.java
+++ src/moped/src/main/java/core/action_strategies/ActionStrategyFactoryImpl.java
@@ -1,4 +1,11 @@
 package core.action_strategies;
 
 public class ActionStrategyFactoryImpl implements ActionStrategyFactory {
+    private static final ActionStrategyFactory INSTANCE = new ActionStrategyFactoryImpl();
+
+    public static ActionStrategyFactory getInstance() {
+        return INSTANCE;
+    }
+
+    private ActionStrategyFactoryImpl() {}
 }
