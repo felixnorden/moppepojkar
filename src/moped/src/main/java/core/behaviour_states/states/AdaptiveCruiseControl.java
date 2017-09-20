9a1a78f12779c2aab575cfe29949a232d5c8d047
Felix Nordén <felixnorden@gmail.com>
2017-09-20 17:42:12 +0200

    Add implementation for instatiation

--- src/moped/src/main/java/core/behaviour_states/states/AdaptiveCruiseControl.java
+++ src/moped/src/main/java/core/behaviour_states/states/AdaptiveCruiseControl.java
@@ -1,15 +1,29 @@
 package core.behaviour_states.states;
 
-import core.action_strategies.ActionStrategy;
+import com_io.CommunicationsMediator;
+import com_io.CommunicatorFactory;
+import core.action_strategies.*;
 
 class AdaptiveCruiseControl implements BehaviourState {
 
-    private ActionStrategy currentStrategy;
+    private BidirectionalHandler currentHandler;
 
-    // May be removed in the near future
-    private ActionStrategy emgyStopStrategy;
+    private final BidirectionalHandler accHandler;
+    private final BidirectionalHandler emergencyHandler;
+    private final BidirectionalHandler manualHandler;
 
     public AdaptiveCruiseControl() {
+
+        CommunicationsMediator comIO = CommunicatorFactory.getComInstance();
+
+        ActionStrategyFactory strategyFactory = ActionStrategyFactoryImpl.getInstance();
+
+        ActionStrategy pidController = strategyFactory.createPIDController();
+        ActionStrategy steerController = new RemoteController(RemoteController.Axis.X, comIO);
+
+        this.accHandler = new BidirectionalHandlerImpl(pidController, steerController);
+
+        ActionStrategy emergencyController = new EmgyStop()
 
     }
 
