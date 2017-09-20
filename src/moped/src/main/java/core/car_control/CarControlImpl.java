0c37d33129c0fe09ee0cf527bfe1987fd0169f47
Unknown <amk1997@hotmail.se>
2017-09-20 22:38:26 +0200

    Implement logic part of car-control

--- src/moped/src/main/java/core/car_control/CarControlImpl.java
+++ src/moped/src/main/java/core/car_control/CarControlImpl.java
@@ -7,31 +7,47 @@ public class CarControlImpl implements CarControl {
 
     @Override
     public int getLastThrottle() {
-        return 0;
+        return lastThrottleValue;
     }
 
     @Override
     public int getLastSteer() {
-        return 0;
+        return lastSteerValue;
     }
 
     @Override
     public void setThrottle(int value) {
-
+        lastThrottleValue = constrainInVCURange(value);
+        // TODO: 20/09/2017 set throttle value to python process
     }
 
     @Override
     public void setSteerValue(int value) {
-
+        lastSteerValue = constrainInVCURange(value);
+        // TODO: 20/09/2017 set steer value to python process
     }
 
     @Override
     public void addThrottle(int value) {
-
+        setThrottle(lastThrottleValue + value);
     }
 
     @Override
     public void addSteer(int value) {
+        setSteerValue(lastSteerValue + value);
+    }
 
+    private int constrainInVCURange(int value) {
+        return constrainInRange(value, -100, 100);
+    }
+    private int constrainInRange(int value, int min, int max) {
+        if (value < min) {
+            value = min;
+        }
+        else if (value > max) {
+            value = max;
+        }
+
+        return value;
     }
 }
