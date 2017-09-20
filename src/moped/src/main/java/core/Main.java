8123e06495b86faf85cf2df7e0499988e24f9d45
Felix Nordén <felixnorden@gmail.com>
2017-09-20 11:11:55 +0200

    Add main loop thread

--- src/moped/src/main/java/core/Main.java
+++ src/moped/src/main/java/core/Main.java
@@ -1,4 +1,27 @@
 package core;
 
+import core.behaviour_states.StateController;
+
 public class Main {
+
+    public static void main(String[] args) {
+        StateController sc = new StateController();
+
+        Thread mainLoop = new Thread(() -> {
+            while (true) {
+                sc.run();
+
+                // Allow for other processes
+                Thread.yield();
+            }
+        });
+
+        mainLoop.start();
+
+        try {
+            mainLoop.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
