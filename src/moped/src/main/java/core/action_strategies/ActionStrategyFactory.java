37811b4820d70b2146799e7688f22472301259de
AntonKimfors <antonkimfors@hotmail.com>
2017-09-20 12:30:46 +0200

    Add implementation requirements

--- src/moped/src/main/java/core/action_strategies/ActionStrategyFactory.java
+++ src/moped/src/main/java/core/action_strategies/ActionStrategyFactory.java
@@ -1,4 +1,8 @@
 package core.action_strategies;
 
 public interface ActionStrategyFactory {
+
+    ActionStrategy createPidParser();
+    ActionStrategy createEmgyStop();
+    ActionStrategy createRemoteController();
 }
--- src/moped/src/main/java/core/action_strategies/ActionStrategyFactoryImpl.java
+++ src/moped/src/main/java/core/action_strategies/ActionStrategyFactoryImpl.java
@@ -1,4 +1,25 @@
 package core.action_strategies;
 
+import com_io.CommunicationsMediator;
+import sun.management.Sensor;
+
 public class ActionStrategyFactoryImpl implements ActionStrategyFactory {
+
+    private CommunicationsMediator appSocket;
+    private Sensor DistanceSensorImpt;
+
+    @Override
+    public ActionStrategy createPidParser() {
+        return null;
+    }
+
+    @Override
+    public ActionStrategy createEmgyStop() {
+        return null;
+    }
+
+    @Override
+    public ActionStrategy createRemoteController() {
+        return null;
+    }
 }
