7d15e20609e81cec3e48fa99f1286bce1c64e03f
NinjaWazzzabi <amk1997@hotmail.se>
2017-09-21 09:48:30 +0200

    Add early implementation of methods

--- src/moped/src/main/java/com_io/CommunicationsMediatorImpl.java
+++ src/moped/src/main/java/com_io/CommunicationsMediatorImpl.java
@@ -2,9 +2,24 @@ package com_io;
 
 import java.util.Map;
 
-abstract class CommunicationsMediatorImpl implements CommunicationsMediator{
+class CommunicationsMediatorImpl implements CommunicationsMediator{
 
     private Map<Direction, DataReceiver> subscribers;
 
     private void directPacket(int[] data, Direction direction){}
+
+    @Override
+    public void transmitData(int[] data, Direction direction) {
+
+    }
+
+    @Override
+    public void subscribe(DataReceiver receiver, Direction direction) {
+
+    }
+
+    @Override
+    public void unsubscribe(DataReceiver receiver) {
+
+    }
 }
--- src/moped/src/main/java/core/behaviour_states/states/AdaptiveCruiseControl.java
+++ src/moped/src/main/java/core/behaviour_states/states/AdaptiveCruiseControl.java
@@ -2,29 +2,39 @@ package core.behaviour_states.states;
 
 import com_io.CommunicationsMediator;
 import com_io.CommunicatorFactory;
+import com_io.CommunicatorFactoryImpl;
 import core.action_strategies.*;
+import core.sensors.DistanceSensor;
+import core.sensors.DistanceSensorImpl;
 
 class AdaptiveCruiseControl implements BehaviourState {
+
+    private static final int EMERGENCY_THRESHOLD = 10;
 
     private BidirectionalHandler currentHandler;
 
     private final BidirectionalHandler accHandler;
     private final BidirectionalHandler emergencyHandler;
-    private final BidirectionalHandler manualHandler;
+
+    private DistanceSensor distanceSensor;
+    private int emergencyCount;
 
     public AdaptiveCruiseControl() {
+        emergencyCount = 0;
 
-        CommunicationsMediator comIO = CommunicatorFactory.getComInstance();
+        CommunicatorFactory comFactory = CommunicatorFactoryImpl.getFactoryInstance();
+        CommunicationsMediator comIO = comFactory.getComInstance();
 
-        ActionStrategyFactory strategyFactory = ActionStrategyFactoryImpl.getInstance();
+        ActionStrategyFactory actionFactory = ActionStrategyFactoryImpl.getInstance();
 
-        ActionStrategy pidController = strategyFactory.createPIDController();
-        ActionStrategy steerController = strategyFactory.createSteerController();
-
+        ActionStrategy pidController = actionFactory.createPIDController();
+        ActionStrategy steerController = actionFactory.createSteerController();
         this.accHandler = new BidirectionalHandlerImpl(pidController, steerController);
 
-        ActionStrategy emergencyController = strategyFactory.createEmgyController();
+        ActionStrategy emergencyController = actionFactory.createEmgyController();
+        this.emergencyHandler = new BidirectionalHandlerImpl(emergencyController, emergencyController);
 
+        this.distanceSensor = DistanceSensorImpl.getInstance();
     }
 
     @Override
@@ -32,8 +42,26 @@ class AdaptiveCruiseControl implements BehaviourState {
 
     }
 
-    // Parameters?
-    private ActionStrategy determineStrategy() {
-        return null;
+    private BidirectionalHandler determineStrategy() {
+        // check range
+        // check brake length for speed, from engine throttle
+        // if brake length is longer than range, increase emergency count.
+        // if counter is over the threshold, set strategy to emergency stop.
+        // else, use acc.
+
+        double range = distanceSensor.getDistance();
+        // TODO: 21/09/2017 Create class to calculate break length.
+        double breakLength = 75; // Temporary value
+
+        if (breakLength > range) {
+            emergencyCount++;
+            if (emergencyCount >= EMERGENCY_THRESHOLD) {
+                return emergencyHandler;
+            }
+        } else {
+            emergencyCount = 0;
+        }
+
+        return accHandler;
     }
 }
