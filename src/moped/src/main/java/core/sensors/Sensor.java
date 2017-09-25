1ef47be380e53d993e3c8fb8fcdca5e7cdc568b2
Felix Nordén <felixnorden@gmail.com>
2017-09-25 12:58:17 +0200

    Add sensor interface

---
+++ src/moped/src/main/java/core/sensors/Sensor.java
@@ -0,0 +1,12 @@
+/*
+ *   Copyright (c) 2017. Felix Nordén
+ *   All rights reservered.
+ *   This code may be used by other parties,
+ *   as long as the used snippets are referenced to the author.
+ *
+ */
+
+package core.sensors;
+
+public interface Sensor {
+}
