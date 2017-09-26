219078e90bab4f0de8ca58fc777144079673f9fd
NinjaWazzzabi <amk1997@hotmail.se>
2017-09-26 11:09:13 +0200

    Add python script usage in DistanceSensor

--- src/moped/src/main/java/core/sensors/DistanceSensorImpl.java
+++ src/moped/src/main/java/core/sensors/DistanceSensorImpl.java
@@ -3,29 +3,74 @@ package core.sensors;
 import core.process_runner.InputSubscriber;
 import core.process_runner.ProcessFactory;
 import core.process_runner.ProcessRunner;
+import sensordataconversion.SensorDataConverter;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
+
+import static java.lang.Double.NaN;
 
 /**
  * Used for reading from the on-board distance sensor.
  */
 public class DistanceSensorImpl implements DistanceSensor, InputSubscriber {
+    private StringBuilder dynamicPythonInput;
+
     private double lastValue;
     private Thread valueLoop;
+    private boolean valueLoopIsKilled;
 
     private static DistanceSensorImpl ourInstance = new DistanceSensorImpl();
     public static DistanceSensorImpl getInstance() {
         return ourInstance;
     }
+    @Override
+    public double getDistance() {
+        return lastValue;
+    }
+
+    @Override
+    public double getValue() {
+        return getDistance();
+    }
+
+    @Override
+    public void kill() {
+        valueLoopIsKilled = true;
+    }
+
+    @Override
+    public synchronized void outputString(String s) {
+        String[] lineNewSplit = s.split("\n");
+        dynamicPythonInput.append(lineNewSplit[0]);
+
+        if (s.contains("\n")) {
+            double temp = new SensorDataConverter().convertDistance(dynamicPythonInput.toString());
+            if (temp != NaN) {
+                lastValue = temp;
+            }
+
+            String[] leftOverLines = Arrays.copyOfRange(lineNewSplit,1, lineNewSplit.length);
+            for (String leftOverLine : leftOverLines) {
+                outputString(leftOverLine);
+            }
+        }
+    }
+
+    private void processPythonInput(String s) {
+
+    }
+
     private DistanceSensorImpl() {
+        valueLoopIsKilled = false;
+        dynamicPythonInput = new StringBuilder();
         lastValue = 100;
 
         valueLoop = new Thread(() -> {
             ProcessRunner sensorData = null;
             try {
-                // TODO: 20/09/2017 Call the right pythonscript to access distance data
-                sensorData = ProcessFactory.createPythonProcess("");
+                sensorData = ProcessFactory.createPythonProcess("run.py");
             } catch (FileNotFoundException e) {
                 System.out.println(e.getMessage());
                 System.out.println("Couldn't start sensor script");
@@ -33,10 +78,10 @@ public class DistanceSensorImpl implements DistanceSensor, InputSubscriber {
 
             sensorData.subscribeToInput(this);
 
-            while (true) {
+            while (!valueLoopIsKilled) {
                 try {
-                    // TODO: 20/09/2017 Use right command to receive data
-                    sensorData.outputToScript("");
+                    sensorData.outputToScript("g.can_ultra\n");
+                    sensorData.flushOutput();
                 } catch (IOException ignored) {
                     break;
                 }
@@ -48,18 +93,6 @@ public class DistanceSensorImpl implements DistanceSensor, InputSubscriber {
                 }
             }
         });
-        //valueLoop.start();
-    }
-
-    @Override
-    public void outputString(String s) {
-        if (s.length() == 3) {
-            lastValue = Integer.valueOf(s);
-        }
-    }
-
-    @Override
-    public double getDistance() {
-        return lastValue;
+        valueLoop.start();
     }
 }
--- src/moped/src/main/java/core/sensors/Sensor.java
+++ src/moped/src/main/java/core/sensors/Sensor.java
@@ -1,6 +1,6 @@
 /*
  *   Copyright (c) 2017. Felix Nordén
- *   All rights reservered.
+ *   All rights reserved.
  *   This code may be used by other parties,
  *   as long as the used snippets are referenced to the author.
  *
@@ -9,4 +9,15 @@
 package core.sensors;
 
 public interface Sensor {
+
+    /**
+     * Gives the current value of the sensor.
+     * @return The current value of the sensor.
+     */
+    double getValue();
+
+    /**
+     * Kills any active thread running in the class.
+     */
+    void kill();
 }
