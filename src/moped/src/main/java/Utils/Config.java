2a15b55a6821fd982197ad2edec7fce85d330136
Hugormen <hugo.ekelund@telia.com>
2017-09-26 11:07:41 +0200

    Add config file. (No haterino plzerino)

---
+++ src/moped/src/main/java/Utils/Config.java
@@ -0,0 +1,11 @@
+package Utils;
+
+public class Config {
+
+    public static double pConstant=0;
+    public static double iRelation=0;
+    public static double dRelation=0;
+    public static double targetDistance=0;
+
+    public static int[] motorValues={1,2,3};
+}
