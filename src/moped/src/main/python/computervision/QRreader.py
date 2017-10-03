301cde292b666e6d345b9a3ce607be3db1082985
Marcus Lindvärn <murk@live.se>
2017-10-03 16:21:21 +0200

    Add comments for the QR-Reader Loop

--- src/moped/src/main/python/computervision/QRreader.py
+++ src/moped/src/main/python/computervision/QRreader.py
@@ -5,11 +5,11 @@ import cv2
 
 
 def main():
-
+    centerPoint = 200
     # Begin capturing video. You can modify what video source to use with VideoCapture's argument. It's currently set
     # to be your webcam.
-    capture = cv2.VideoCapture(0)
 
+    capture = cv2.VideoCapture(0)
     while True:
         # To quit this program press q.
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -26,7 +26,9 @@ def main():
 
         # Uses PIL to convert the grayscale image into a ndary array that ZBar can understand.
         image = Image.fromarray(gray)
+        # Saves the width and height from the image
         width, height = image.size
+        # converts the image to (for zbar) readable bytes
         zbar_image = zbar.Image(width, height, 'Y800', image.tobytes())
 
         # Scans the zbar image.
@@ -35,14 +37,18 @@ def main():
 
         # Prints data from image.
         for decoded in zbar_image:
-            print(decoded.data)
-            topLeftCorners, bottomLeftCorners, bottomRightCorners, topRightCorners = [item for item in decoded.location]
-            centerPointQR = (bottomLeftCorners[0] + bottomRightCorners)[0] / 2
-            centerPoint = width/2
-            #Postive = left of target
-            distanceFromCenter = centerPoint - centerPointQR
 
-            
+            #print(decoded.data)
+
+            #saves The corners of the read QR-Code
+            topLeftCorners, bottomLeftCorners, bottomRightCorners, topRightCorners = [item for item in decoded.location]
+            #calculatesthe center fro m2 given xCoordinates
+            centerPointQR = (bottomLeftCorners[0] + bottomRightCorners)[0] / 2
+            #distanceFromCenter saves the distance given in pixels from the centerof the picture to the cetner of the QR-Code Postive = left of target
+            distanceFromCenter = centerPoint - centerPointQR
+            #Prints the difference between center of the image and the center of the QR code
+            print(distanceFromCenter)
+            '''   testkod nedan.
             print("Top left xy")
             print(topLeftCorners)
             print("Bottom left xy")
@@ -51,11 +57,9 @@ def main():
             print(topRightCorners)
             print("Bottom right xy")
             print(bottomRightCorners)
-            
-            
-            
+            '''
 
-    
-        
+
 if __name__ == "__main__":
     main()
+
