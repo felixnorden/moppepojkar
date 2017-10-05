f993b249724ebc7faf0316515fd2644f2a7d84b6
NinjaWazzzabi <amk1997@hotmail.se>
2017-10-05 15:06:47 +0200

    Add default values in config
    asd

--- src/moped/src/main/java/utils/Config.java
+++ src/moped/src/main/java/utils/Config.java
@@ -2,10 +2,20 @@ package utils;
 
 public class Config {
 
-    public static double pConstant=0;
-    public static double iRelation=0;
-    public static double dRelation=0;
-    public static double targetDistance=0;
+    public static final String QR_PATH = "QReader.py";
 
-    public static int[] motorValues={0,7,11,15,19,23,27,37};
+    public static final int ACC_UPDATE_DELAY = 100;
+    public static final double ACC_TGT_DIST = 0.3;
+    public static final double ACC_P = 70;
+    public static final double ACC_I = 0;
+    public static final double ACC_D = 1.5;
+
+    public static final int LAT_UPDATE_DELAY = 100;
+    public static final double LAT_TGT_POS = 0;
+    public static final double LAT_P = 10;
+    public static final double LAT_I = 0;
+    public static final double LAT_D = 0;
+
+
+    public static int[] motorValues = {0, 7, 11, 15, 19, 23, 27, 37};
 }
