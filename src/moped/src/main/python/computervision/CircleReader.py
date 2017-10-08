53dcace168164cf79e8b99d6f2b5643bc1868628
Zumpo <carlsson889@gmail.com>
2017-10-09 00:36:35 +0200

    Added threading and a new way to track circles

--- src/moped/src/main/python/computervision/CircleReader.py
+++ src/moped/src/main/python/computervision/CircleReader.py
@@ -1,50 +1,124 @@
+# import the necessary packages
+from __future__ import print_function
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import imutils
+import time
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(0)
+import zbar
+import pydoc
+from PIL import Image
+
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from threading import Thread
+import cv2
+
+class PiVideoStream:
+    def __init__(self, resolution=(480, 320), framerate=10):
+        # initialize the camera and stream
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture,
+                                                     format="bgr", use_video_port=True)
+
+        # initialize the frame and the variable used to indicate
+        # if the thread should be stopped
+        self.frame = None
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        t = Thread(target=self.update, args=())
+        t.daemon = True
+        t.start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        for f in self.stream:
+            # grab the frame from the stream and clear the stream in
+            # preparation for the next frame
+            self.frame = f.array
+            self.rawCapture.truncate(0)
+
+            # if the thread indicator variable is set, stop the thread
+            # and resource camera resources
+            if self.stopped:
+                self.stream.close()
+                self.rawCapture.close()
+                self.camera.close()
+                return
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
+
+
+# created a *threaded *video stream, allow the camera sensor to warmup,
+# and start the FPS counter
+print("[INFO] sampling THREADED frames from `picamera` module...")
+vs = PiVideoStream().start()
+time.sleep(2.0)
+
+greenLower = (29, 86, 20)
+greenUpper = (64, 255, 200)
 while(1):
+    frame = vs.read()
+    # width=400
 
-    # Take each frame
-    _, frame = cap.read()
 
-    # Convert BGR to HSV
-
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # define range of blue color in HSV 
-    lower_green = np.array([50,50,120]) 
-    upper_green = np.array([70,255,255])
+    # construct a mask for the color "green", then perform
+    # a series of dilations and erosions to remove any small
+    # blobs left in the mask
+    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
 
-    imgThreshHigh = cv2.inRange(hsv, lower_green, upper_green)
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
 
-    imgray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    thresh = 18
-    edges = cv2.Canny(imgray,thresh,thresh*3)
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-    circles = cv2.HoughCircles(imgThreshHigh, cv2.HOUGH_GRADIENT, 1, 500, 25, 75, 5, 15)
-    maxRadius= 0
-    xc = 0.00
-    yc = 0.00
-    found = False 
-    if circles is not None:
-        found = True
-        for i in circles[0,:3]:
-            if i[2] < 100:
-                if i[2] > maxRadius:
-                    maxRadius = i[2]
-                    if maxRadius > 1.0:
-                        # draw the outer circle
-                        cv2.circle(frame,(i[0],i[1]),maxRadius,(0,0,255),2)
-                        # draw the center of the circle
-                        cv2.circle(frame,(i[0],i[1]),1,(0,0,255),3)
-                        xc = i[0]
-                        yc = i[1] 
-    if found: 
-        print "ball detected at position:",xc, ",", yc, " with radius:", maxRadius
-    else: 
-        print "no ball" 
-    cv2.imshow('frame',frame)
-    cv2.imshow('edges',edges)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
+        # only proceed if the radius meets a minimum size
+        if radius > 70:
+            print(str(center[0] - 480/2) +  "::: " + str(radius))
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            # cv2.circle(frame, (int(x), int(y)), int(radius),
+            #            (0, 255, 255), 2)
+            # cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            # pts.appendleft(center)
+
+
+
+    # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+    #
+    # # ensure at least some circles were found
+    # if circles is not None:
+    #     print(circles[0][0][0] - 480/2)
 cv2.destroyAllWindows()
+vs.stop()
