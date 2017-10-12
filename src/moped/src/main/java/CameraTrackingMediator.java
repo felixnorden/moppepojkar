80bdff11a4b5308e0f607d5b921ce64fe5dac59b
NinjaWazzzabi <amk1997@hotmail.se>
2017-10-12 13:45:53 +0200

    Fix error on reading input from CircleReader

--- src/moped/src/main/java/CameraTrackingMediator.java
+++ src/moped/src/main/java/CameraTrackingMediator.java
@@ -3,27 +3,38 @@ import com_io.Direction;
 import core.process_runner.ProcessRunner;
 import utils.Config;
 
+import java.util.Arrays;
+
 class CameraTrackingMediator {
 
     private final ProcessRunner processRunner;
     private CommunicationsMediator communicationsMediator;
+    private StringBuilder stringBuilder;
 
     CameraTrackingMediator(CommunicationsMediator communicationsMediator, ProcessRunner processRunner) {
         this.communicationsMediator = communicationsMediator;
         this.processRunner = processRunner;
+        this.stringBuilder = new StringBuilder();
 
-        processRunner.subscribeToInput(this::stringRecevied);
+        processRunner.subscribeToInput(this::stringReceived);
         processRunner.start();
     }
 
-    void stringRecevied(String string) {
-        String[] formattedData = string.split(Config.REGEX);
-        if (formattedData.length == 2) {
-            String targetOffset  = formattedData[0];
-            String targetDistance  = formattedData[1];
+    void stringReceived(String string) {
+        for (char c : string.toCharArray()) {
+            if (c != 10 && c != 13) {
+                stringBuilder.append(c);
+            } else {
+                String[] formattedData = stringBuilder.toString().split(Config.REGEX);
+                if (formattedData.length == 2) {
+                    String targetOffset  = formattedData[0];
+                    String targetDistance  = formattedData[1];
 
-            communicationsMediator.transmitData(Config.CAM_TGT_OFFSET + Config.REGEX + targetOffset, Direction.INTERNAL);
-            communicationsMediator.transmitData(Config.CAM_TGT_DIST + Config.REGEX + targetDistance, Direction.INTERNAL);
+                    communicationsMediator.transmitData(Config.CAM_TGT_OFFSET + Config.REGEX + targetOffset, Direction.INTERNAL);
+                    communicationsMediator.transmitData(Config.CAM_TGT_DIST + Config.REGEX + targetDistance, Direction.INTERNAL);
+                }
+                stringBuilder.delete(0, stringBuilder.length());
+            }
         }
     }
 
