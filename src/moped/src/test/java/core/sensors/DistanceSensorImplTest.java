8b17ac3a0c0d2c3a5ec66f0b3a3215311cf20339
NinjaWazzzabi <amk1997@hotmail.se>
2017-10-12 13:56:52 +0200

    Add tests to the distance sensor

--- src/moped/src/test/java/core/sensors/DistanceSensorImplTest.java
+++ src/moped/src/test/java/core/sensors/DistanceSensorImplTest.java
@@ -1,16 +1,77 @@
 package core.sensors;
 
+import arduino.ArduinoCommunicator;
+import arduino.ArduinoListener;
+import com_io.CommunicationsMediator;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 class DistanceSensorImplTest {
-    private DistanceSensor sensorInstance;
+
+    private ArduinoCommunicator ardMock;
+    private CommunicationsMediator comMock;
+
+    private ArduinoListener arduinoListener;
+    private DistanceSensorImpl sensorInstance;
+
+
+    @Test
+    void getDistance() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    @DisplayName("should subscribe a consumer")
+    void subscribe() {
+        Consumer<Double> consumer = new ConsumerImpl();
+        Consumer<Double> consumerSpy = spy(consumer);
+
+        StringBuilder sb = new StringBuilder();
+        sensorInstance.subscribe(consumerSpy);
+        sensorInstance.receivedString("0.3\n", sb);
+
+        verify(consumerSpy, times(1)).accept(any());
+    }
+
+    @Test
+    void unsubscribe() {
+    }
+
+    @Test
+    @DisplayName("should build string from input")
+    void receivedString() {
+
+        StringBuilder sb = new StringBuilder();
+        sensorInstance.receivedString("100", sb);
+
+        assertEquals("100", sb.toString());
+    }
+
+    @Test
+    @DisplayName("should reset the StringBuilder on new line")
+    void receivedString2() {
+        StringBuilder sb = new StringBuilder();
+        sensorInstance.receivedString("100\n", sb);
+
+        assertEquals("", sb.toString());
+    }
 
     @BeforeEach
     void setUp() {
-        sensorInstance = SensorRepository.getDistanceSensor();
+        comMock = mock(CommunicationsMediator.class);
+        ardMock = mock(ArduinoCommunicator.class);
 
+        sensorInstance = new DistanceSensorImpl(comMock, ardMock);
     }
 
     @AfterEach
@@ -20,6 +81,15 @@ class DistanceSensorImplTest {
     @Test
     void getInstance() {
         assert sensorInstance != null;
+    }
+
+    class ConsumerImpl implements Consumer<Double>{
+        Double d;
+
+        @Override
+        public void accept(Double aDouble) {
+            this.d = 1.0;
+        }
     }
 
 }
