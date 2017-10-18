83dca93733a48c076ca56882d5a3c69410802777
Emil Jansson <emiljanQ3@hotmail.com>
2017-10-18 12:02:02 +0200

    FIX the QuickChangeFilterTest so that the tests passes

--- src/moped/src/test/java/core/sensors/QuickChangeFilterTest.java
+++ src/moped/src/test/java/core/sensors/QuickChangeFilterTest.java
@@ -9,12 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * Created by Emil Jansson on 2017-10-17.
  */
 public class QuickChangeFilterTest {
-    final int maxQueueSize = 10;
-    QuickChangeFilter filter = new QuickChangeFilter(10,maxQueueSize);
+
 
     @Test
     public void filterValue() {
-
+        final int maxQueueSize = 10;
+        QuickChangeFilter filter = new QuickChangeFilter(10,maxQueueSize);
         double[] dataSet = {1,2,3,17,5,6,-18,8,9,10};
         for (int i = 0; i < maxQueueSize; i++) {
             assertTrue(dataSet[i] == filter.filterValue(dataSet[i]));
@@ -24,24 +24,38 @@ public class QuickChangeFilterTest {
 
     @Test
     public void filterValue1() {
+        final int maxQueueSize = 10;
+        QuickChangeFilter filter = new QuickChangeFilter(10,maxQueueSize);
         double[] dataSet = {1,2,3,50,5,6,-50,8,9,10};
+        for (int i = 0; i < maxQueueSize; i++) {
+            assertTrue(dataSet[i] == filter.filterValue(dataSet[i]));
+        }
         for (double d: dataSet) {
-            assertTrue(filter.filterValue(d)<=10 && filter.filterValue(d)>=1);
+            double filteredValue = filter.filterValue(d);
+            System.out.println("Raw: "+d+"  Filtered: " + filteredValue);
+            assertTrue(filteredValue<=10 && filteredValue>=1);
         }
     }
 
     @Test
     public void filterValue2(){
+        final int maxQueueSize = 10;
+        QuickChangeFilter filter = new QuickChangeFilter(10,maxQueueSize);
         double[] zeroSet = {0,0,0,0,0,0,0,0,0,0};
         for(double d: zeroSet){
-            filter.filterValue(d);
+            double filteredValue = filter.filterValue(d);
+            System.out.println("Raw: "+d+"  Filtered: " + filteredValue);
         }
-        double[] giantSet = {100,100,100,100,100,100,100,100,100,100};
-        for (int i = 0; i < maxQueueSize/2; i++){
-            assertTrue(filter.filterValue(giantSet[i]) == 0);
+        double[] giantSet = {20,20,20,20,20,20,20,20,20,20};
+        for (int i = 0; i < maxQueueSize/2 +1; i++){
+            double filteredValue = filter.filterValue(giantSet[i]);
+            System.out.println("Raw: "+giantSet[i]+"  Filtered: " + filteredValue);
+            assertTrue(filteredValue == 0);
         }
-        for (int i = 0; i < maxQueueSize/2; i++){
-            assertTrue(filter.filterValue(giantSet[i]) == 100);
+        for (int i = 0; i < maxQueueSize/2 -1; i++){
+            double filteredValue = filter.filterValue(giantSet[i]);
+            System.out.println("Raw: "+giantSet[i]+"  Filtered: " + filteredValue);
+            assertTrue(filteredValue == 20);
         }
     }
 
