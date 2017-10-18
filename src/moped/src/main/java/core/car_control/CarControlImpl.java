5a8526a246fa7751eb82b8b83eee3a385377fde8
NinjaWazzzabi <amk1997@hotmail.se>
2017-10-18 14:32:14 +0200

    Edit lower delay between iterations updating the Arduino

--- src/moped/src/main/java/core/car_control/CarControlImpl.java
+++ src/moped/src/main/java/core/car_control/CarControlImpl.java
@@ -37,7 +37,7 @@ public class CarControlImpl implements CarControl {
                 }
                 sendValuesToCar();
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
